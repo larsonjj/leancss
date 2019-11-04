3f0750168d791764f192900f18db669adc86b9ff
Jake Larson <jake.j.larson@gmail.com>
2019-11-04 16:09:26 -0600

    Built out gulp system

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const util = require('gulp-util');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+
+const isProduction = util.env.production;
+const paths = {
+  src: './src/lean.scss',
+  dist: './dist',
+  sass: './src/**/*.scss'
+}
+
+gulp.task('sass', () => {
+  return gulp
+    .src(paths.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: isProduction ? 'compressed' : 'expanded'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('clean', () => {
+  return del([paths.dist]);
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.sass, done => {
+    gulp.series(['sass'])(done);
+  });
+});
+
+gulp.task('default', gulp.series(['clean', 'sass']));
